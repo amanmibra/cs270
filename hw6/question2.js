8341154fdff18d89d67c847d2e736438c9468c6d
Aman Ibrahim <amanmibra@gmail.com>
2019-04-26 12:26:13 -0400

    print out policies for q2

--- hw6/question2.js
+++ hw6/question2.js
@@ -13,25 +13,31 @@ for (var i = 0; i < 59; i++) {
 
 function getTVals(i) {
   return [
-    2 + (0.9 * T[i] + 0.1 * R[i]),
-    3 + (0.7 * T[i] + 0.3 * R[i]),
+    2 + 0.8 * (0.9 * T[i] + 0.1 * R[i]),
+    3 + 0.8 * (0.7 * T[i] + 0.3 * R[i]),
   ];
 }
 
 function getRVals(i) {
   return [
-    (0.6 * R[i] + 0.3 * T[i] + 0.1 * B[i]),
-    1 + (1 * B[i]),
+    0.8 * (0.6 * R[i] + 0.3 * T[i] + 0.1 * B[i]),
+    1 + 0.8 * (1 * B[i]),
   ];
 }
 
 function getBVals(i) {
   return [
-    (0.4 * B[i] + 0.6 * T[i]),
-    1 + (B[i]),
+    0.8 * (0.4 * B[i] + 0.6 * T[i]),
+    1 + 0.8 * (B[i]),
   ];
 }
 
+console.log('----values----');
 console.log('T values:', T);
 console.log('R values:', R);
 console.log('B values:', B);
+
+console.log('----policies (D v. DD)----');
+console.log('T:', getTVals(59));
+console.log('R:', getRVals(59));
+console.log('B:', getBVals(59));
